ef42a956c5c2614031a746a67dd84e67af540923
Noh <aeonsse@live.com>
2018-05-31 15:49:55 +0200

    Fixing Esse3Api, adding data classes

---
+++ Esse3Api/src/data/Esse3Block.java
@@ -0,0 +1,32 @@
+package data;
+
+public class Esse3Block {
+	private String blockName;
+	private String totalHours;
+	
+	
+	public Esse3Block() {
+	}
+	
+	public Esse3Block(String blockName, String totalHours) {
+		this.setBlockName(blockName);
+		this.setTotalHours(totalHours);
+	}
+	
+	public String getBlockName() {
+		return blockName;
+	}
+	public void setBlockName(String blockName) {
+		this.blockName = blockName;
+	}
+	public String getTotalHours() {
+		return totalHours;
+	}
+	public void setTotalHours(String totalHours) {
+		this.totalHours = totalHours;
+	}
+	
+		
+	
+
+}
---
+++ Esse3Api/src/data/Esse3Subj.java
@@ -0,0 +1,43 @@
+package data;
+
+public class Esse3Subj {
+	private String subjName;
+	private String subjHours;
+	private String blockName;
+	
+	public Esse3Subj() {
+	}
+	
+	public Esse3Subj(String subjName, String subjHours, String blockName) {
+		this.subjName = subjName;
+		this.subjHours = subjHours;
+		this.blockName = blockName;
+	}
+	
+	public String getSubjName() {
+		return subjName;
+	}
+	
+	public void setSubjName(String subjName) {
+		this.subjName = subjName;
+	}
+	
+	public String getSubjHours() {
+		return subjHours;
+	}
+	
+	public void setSubjHours(String subjHours) {
+		this.subjHours = subjHours;
+	}
+	
+	public String getBlockName() {
+		return blockName;
+	}
+	
+	public void setBlockName(String blockName) {
+		this.blockName = blockName;
+	}
+	
+	
+
+}
